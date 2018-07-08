5bd77fbbbed9c522f6f4bcae0e74fd016a194092
Suraj Pokhrel <mrsurajpokhrel@gmail.com>
2018-07-08 11:47:21 +1000

    All page flow completed

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import ContactMe from './ContactMe';
+import AboutMe from './AboutMe';
+import Home from './Home';
+import News from './components/news/News';
+import Portfolio from './components/portfolio/portfolio'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
